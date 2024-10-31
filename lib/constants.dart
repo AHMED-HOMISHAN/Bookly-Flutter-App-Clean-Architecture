d28525956677cf80c44a4b2193f235fefdc5ff1a
AHMED-HOMISHAN <ahmedsameer.ye@gmail.com>
2024-10-31 20:43:10 +0300

    Add kFeaturedBox constant for featured books & register adapter and open featured box

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color.fromARGB(255, 42, 39, 51);
 const kTranstionDuration = Duration(milliseconds: 250);
 const kGtSectraFine = 'GT Sectra Fine';
+const kFeaturedBox= 'featured_box';
