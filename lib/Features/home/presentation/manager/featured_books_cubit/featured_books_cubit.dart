0ed349b33acbac7d74f818ba17d3d41dd2e261f5
AHMED-HOMISHAN <ahmedsameer.ye@gmail.com>
2024-11-08 14:40:09 +0300

    Implement fetchFeaturedBooks method in FeaturedBooksCubit with error handling

--- lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:meta/meta.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInital());
+  FeaturedBooksCubit(this.fetchFeaturedBooksUseCase)
+      : super(FeaturedBooksInital());
+
+  final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksInital());
+    var result = await fetchFeaturedBooksUseCase.call();
+    result.fold((failure) {
+      emit(FeaturedBooksFailur(errorMessage: failure.message));
+    }, (data) {
+      emit(FeaturedBooksSuccess(books: data));
+    });
+  }
 }
