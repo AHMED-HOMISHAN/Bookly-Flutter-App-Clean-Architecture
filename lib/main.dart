53a8f66c4619e841e2e7ed495df5f249a05cc5be
AHMED-HOMISHAN <ahmedsameer.ye@gmail.com>
2024-11-08 15:07:29 +0300

    Refactor main.dart to use service locator for dependency injection; add setupServiceLocator function and update Hive box data saving method

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:bookly/Features/home/data/data_sources/home_local_data_source.dart';
-import 'package:bookly/Features/home/data/data_sources/home_remote_data_source.dart';
 import 'package:bookly/Features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
@@ -7,9 +5,8 @@ import 'package:bookly/Features/home/domain/use_cases/fetch_newest_books_use_cas
 import 'package:bookly/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly/Features/home/presentation/manager/newst_books_cubit/newset_books_cubit.dart';
 import 'package:bookly/constants.dart';
-import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/core/utils/app_router.dart';
-import 'package:dio/dio.dart';
+import 'package:bookly/core/utils/functions/setupServiceLocator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -18,6 +15,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
+  setupServiceLocator();
   await Hive.openBox<BookEntity>(kFeaturedBox);
   await Hive.openBox<BookEntity>(kNewestBox);
   runApp(const Bookly());
@@ -32,16 +30,10 @@ class Bookly extends StatelessWidget {
       providers: [
         BlocProvider(
             create: (context) => FeaturedBooksCubit(FetchFeaturedBooksUseCase(
-                homeRepo: HomeRepoImpl(
-                    homeRemoteDataSource:
-                        HomeRemoteDataSourceImpl(ApiService(Dio())),
-                    homeLocalDataSource: HomeLocalDataSourceImpl())))),
+                homeRepo: getIt.get<HomeRepoImpl>()))),
         BlocProvider(
-            create: (context) => NewsetBooksCubit(FetchNewestBooksUseCase(
-                homeRepo: HomeRepoImpl(
-                    homeRemoteDataSource:
-                        HomeRemoteDataSourceImpl(ApiService(Dio())),
-                    homeLocalDataSource: HomeLocalDataSourceImpl()))))
+            create: (context) => NewsetBooksCubit(
+                FetchNewestBooksUseCase(homeRepo: getIt.get<HomeRepoImpl>())))
       ],
       child: MaterialApp.router(
         routerConfig: AppRouter.router,
