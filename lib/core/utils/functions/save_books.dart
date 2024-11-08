0a89a3ff8e9caa9878af7327d3f0954662bd358c
AHMED-HOMISHAN <ahmedsameer.ye@gmail.com>
2024-11-08 15:38:44 +0300

    Add FeaturedBooksListViewBlocBuilder for BLoC state management and update saveBoxData function for type safety

--- lib/core/utils/functions/save_books.dart
+++ lib/core/utils/functions/save_books.dart
@@ -1,7 +1,7 @@
- import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:hive/hive.dart';
 
-  void saveBoxData(String boxName, List<BookEntity> books) {
-    var box = Hive.box(boxName);
-    box.addAll(books);
-  }
+void saveBoxData(String boxName, List<BookEntity> books) {
+  var box = Hive.box<BookEntity>(boxName);
+  box.addAll(books);
+}
